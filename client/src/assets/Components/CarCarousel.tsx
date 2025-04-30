1c4a590103673cc2507f4ea3dec75ea5faf228d2
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-30 19:34:25 +0000

    styling in progress for the main page.

--- client/src/assets/Components/CarCarousel.tsx
+++ client/src/assets/Components/CarCarousel.tsx
@@ -1,11 +1,17 @@
 import { PiArrowFatLeftLight, PiArrowFatRightLight } from 'react-icons/pi';
+import './CarCarousel.css';
 
 export function CarCarousel() {
   return (
-    <div className="carousel">
-      <PiArrowFatLeftLight />
-      <img />
-      <PiArrowFatRightLight />
-    </div>
+    // <div className="page-container">
+    <>
+      <PiArrowFatLeftLight className="arrow-color-left" size={200} />
+      <div className="image-container">
+        <img alt="car image here" src="/images/2001-lexus-gs-430-black.jpg" />
+        {/* image here... */}
+      </div>
+      <PiArrowFatRightLight size={200} className="arrow-color-right" />
+    </>
+    // </div>
   );
 }
--- client/src/assets/Pages/HomePage.tsx
+++ client/src/assets/Pages/HomePage.tsx
@@ -1,10 +1,21 @@
 import { CarCarousel } from '../Components/CarCarousel';
 import '../Pages/HomePage.css';
+import { FaRegCircleDot, FaRegCircle } from 'react-icons/fa6';
 
 export function HomePage() {
   return (
-    <div className="bg-green">
+    // <div>
+    <div className="homepage-container">
       <CarCarousel />
+      <div className="circle-container">
+        <div className="circle-spacing">
+          <FaRegCircleDot size={32} />
+        </div>
+        <div className="circle-spacing">
+          <FaRegCircle size={32} />
+        </div>
+      </div>
+      {/* </div> */}
     </div>
   );
 }
