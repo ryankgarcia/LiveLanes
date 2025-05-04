f90a6ebddd9d3027d9a0a65b624a9f26cab8733e
Ryan Garcia <ryankgarcia22@gmail.com>
2025-05-04 07:13:41 +0000

    formatting the my purchases page.

--- client/src/assets/Pages/MyPurchases.tsx
+++ client/src/assets/Pages/MyPurchases.tsx
@@ -1,3 +1,23 @@
+import './MyPurchasesLayout.css';
+
 export function MyPurchases() {
-  return <h2>MyPurchases JSX here...</h2>;
+  return (
+    <div className="page-color">
+      <div className="mypurchases-container">
+        <div className="mypurchase-row">
+          <div className="mypurchase-column-main">
+            <h2 className="final-sale-text">
+              Congratulations, you are the highest bidder!
+            </h2>
+            <h2 className="final-sale-text">Please continue to checkout...</h2>
+            <div className="final-sale-card"></div>
+            <div className="mypurchase-column-side">
+              <div className="checkout-summary-box"></div>
+              <div className="checkout-box"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
