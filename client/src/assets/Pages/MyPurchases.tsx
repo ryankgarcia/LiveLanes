e2ab0cd76de8f45ff03ca1cb45b6546cce73a75f
Ryan Garcia <ryankgarcia22@gmail.com>
2025-05-04 08:43:09 +0000

    made a few css changes to the checkout page.

--- client/src/assets/Pages/MyPurchases.tsx
+++ client/src/assets/Pages/MyPurchases.tsx
@@ -1,3 +1,4 @@
+import { RiLock2Fill } from 'react-icons/ri';
 import './MyPurchasesLayout.css';
 
 export function MyPurchases() {
@@ -11,12 +12,27 @@ export function MyPurchases() {
             </h2>
             <h2 className="final-sale-text">Please continue to checkout...</h2>
             <div className="final-sale-card"></div>
-            <div className="mypurchase-column-side">
-              <div className="checkout-summary-box"></div>
-              <div className="checkout-box"></div>
+            <div className="checkout-summary-box">
+              <h2>Checkout Summary</h2>
+              <h3 className="checkout-summary-chargers final-prices">
+                Closing Bid:
+              </h3>{' '}
+              <span className="checkout-summary-cost final charges">
+                $4,500
+              </span>
+              <h3>Buyer Fee:</h3> <span>$340</span>
+              <h3>Subtotal:</h3> <span>$4,840</span>
+            </div>
+            <div className="checkout-box">
+              <h4>Transportation:</h4> <span>$180</span>
             </div>
           </div>
+          <h4 className="final-charges">Total:</h4>
+          <span className="final-prices">$5,020</span>
         </div>
+        <button className="checkout-button">
+          Begin Checkout {<RiLock2Fill color="white" />}
+        </button>
       </div>
     </div>
   );
